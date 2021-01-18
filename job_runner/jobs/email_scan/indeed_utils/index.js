563130eff0b19b44a7cd967e8004dba28f14f486
neurocollective <david@neurocollective.io>
2021-01-18 15:44:48 -0500

    a few lint fixes

--- job_runner/jobs/email_scan/indeed_utils/index.js
+++ job_runner/jobs/email_scan/indeed_utils/index.js
@@ -1,33 +1,32 @@
+/* eslint no-multi-spaces: 0 */
 import { JSDOM } from 'jsdom';
 
 const DOC_TYPE_HTML = '<!DOCTYPE html';
 
 const JOB_TITLE_SELECTOR =    `.corner > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) >
-	table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) >
-	tbody:nth-child(1) > tr:nth-child(6) > td:nth-child(1) > a:nth-child(1)`;
+  table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) >
+  tbody:nth-child(1) > tr:nth-child(6) > td:nth-child(1) > a:nth-child(1)`;
 
 const ENTITIY_NAME_SELECTOR = `.corner > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) >
-	table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) >
-	tbody:nth-child(1) > tr:nth-child(7) > td:nth-child(1) > p:nth-child(1) > span:nth-child(1) > a:nth-child(1)`;
+  table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) >
+  tbody:nth-child(1) > tr:nth-child(7) > td:nth-child(1) > p:nth-child(1) > span:nth-child(1) > a:nth-child(1)`;
 
 const LOCATION_SELECTOR =     `.corner > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) >
-	table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) >
-	tbody:nth-child(1) > tr:nth-child(7) > td:nth-child(1) > p:nth-child(1) > span:nth-child(2)`;
+  table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) >
+  tbody:nth-child(1) > tr:nth-child(7) > td:nth-child(1) > p:nth-child(1) > span:nth-child(2)`;
 
 export const scanIndeedEmail = (rawEmailString = '') => {
+  const indexOfDocType = rawEmailString.indexof(DOC_TYPE_HTML);
 
-	const indexOfDocType = rawEmailString.indexof(DOC_TYPE_HTML);
+  if (indexOfDocType < 0) {
+    const msg = `WARNING: indeed email did not start with ${DOC_TYPE_HTML} or has no html`;
+    console.log(msg);
+    return { error: msg };
+  }
 
-	if (indexOfDocType < 0) {
-		const msg = `WARNING: indeed email did not start with ${DOC_TYPE_HTML} or has no html`;
-		console.log(msg);
-		return { error: msg };
-	}
+  const htmlOnly = rawEmailString.slice(indexOfDocType);
 
-	const htmlOnly = rawEmailString.slice(indexOfDocType);
-
-	const { window: { document } } = new JSDOM(htmlOnly);
-
+  const { window: { document } } = new JSDOM(htmlOnly);
 
   return { error: null };
 };
